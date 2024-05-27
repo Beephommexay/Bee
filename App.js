7252596a3a7ed4fa08af4e5b922f34d0c7bde576
unknown <beephommexay19@gmail.com>
2024-05-27 13:18:01 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import CounterApp from './counter';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Phetsamay 2CW1</Text>
+      <StatusBar style="auto" />
+      <CounterApp></CounterApp>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
